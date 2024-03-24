f68e94352879470427f51431762f18a49f61df52
Koushik Dandapat <koushikdandapat16@gmail.com>
2024-03-24 19:34:25 +0530

    second

--- server.js
+++ server.js
@@ -22,39 +22,113 @@ app.use(express.json());
 
 const apikey = process.env.APIKEY;
 // POST endpoint to receive locations and optimize route
+
+
 app.post('/',  async(req, res) => {
   try {
-   const data  = req.body.markers;
-   const n = data.length;
-  
-  
-   const r =  await callApi(data , n);
-  const [path , distance] =   geneticAlgorithm(r , n * 10 ,n * 20 , 4, 0.8 , 0.2 );
-  const bestpath = [];
-  for(let i= 0 ;i < n ; i++){
-    bestpath.push(data[path[i]]);
-  }
+   const warehouse  = req.body.markers;
+   const riders = req.body.rider;
+   const users = req.body.users;
+   const wtu = await findwtu(warehouse , users) ;
+   const rtw = await findrtw(riders , warehouse);
+    let dis = Number.MAX_SAFE_INTEGER;
+    let w ;
+    let u;
+    let r;
+   for(let i =0; i < rtw.length ; i++) {
+    for(let j = 0 ; j < wtu.length ; j++) {
+      for(let k =0 ; k < wtu[0].length ; k++) {
+          let curr = rtw[i][j] + wtu[j][k];
+          if(curr < dis) {
+            dis = curr;
+            w = j;
+            r = i;
+            u = k;
+          }
+      }
+    }
+   }
 
-  
-   res.json({bestpath , distance});
 
-  
-   
-   
+   let locations = [];
+   locations.push(users[u]);
+   for(let i= 0 ;i < users.length ; i++) {
+    if(users[i] === users[u]) continue;
+    else locations.push(users[i]);
+   }
+   let n = locations.length;
+   const utu = await callApi(locations , n);
+   const [path , distance] =   geneticAlgorithm(utu , n * 10 ,n * 20 , 4, 0.8 , 0.2 );
+   let anspath = [];
+   anspath.push(riders[r]);
+   anspath.push(warehouse[w]);
+   for(let i = 0 ; i < path.length ; i++) {
+    anspath.push(locations[path[i]]);
+   }
+    console.log(dis);
+    console.log(distance);
+   let ansdis = dis + distance;
+   console.log(ansdis);
 
-  
    
+ 
+   res.json({anspath , ansdis});
+
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
+async function findwtu(warehouse , users) {
+  let graph = Array.from(Array(warehouse.length), () => new Array(users.length));
+  for(let i = 0 ; i < warehouse.length ; i++) {
+    for(let j = 0; j < users.length ; j++) { 
+      const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${warehouse[i].lat},${warehouse[i].lng}&destinations=${users[j].lat},${users[j].lng}&key=${apikey}`;
+      await axios.get(url)
+    .then(async (response) => {
+     const dis =  response.data.rows[0].elements[0].distance.text;
+     graph[i][j] = parseInt(dis.replace(/\D/g, ''), 10);
+    
+    }).catch(error => {
+      console.error(error);
+      throw new Error('Error fetching distance data');
+    })  ;
+
+    return graph;
+
+    }
+  }
+}
+
+
+async function findrtw(riders , warehouse) {
+  let graph = Array.from(Array(riders.length), () => new Array(warehouse.length));
+  for(let i = 0 ; i < riders.length ; i++) {
+    for(let j = 0; j < warehouse.length ; j++) { 
+      const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${riders[i].lat},${riders[i].lng}&destinations=${warehouse[j].lat},${warehouse[j].lng}&key=${apikey}`;
+      await axios.get(url)
+    .then(async (response) => {
+     const dis =  response.data.rows[0].elements[0].distance.text;
+     graph[i][j] = parseInt(dis.replace(/\D/g, ''), 10);
+    
+    }).catch(error => {
+      console.error(error);
+      throw new Error('Error fetching distance data');
+    })  ;
+
+    return graph;
+
+    }
+  }
+}
+
+
  async function callApi(data , n) {
   let graph = Array.from(Array(n), () => new Array(n));
   for(let i = 0 ; i < n ; i++) {
-   for(let j = 0; j < n ; j++) {
-     
+   for(let j = 0; j < n ; j++) { 
      if(i !== j && j > i) {
     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${data[i].lat},${data[i].lng}&destinations=${data[j].lat},${data[j].lng}&key=${apikey}`;
        await axios.get(url)
@@ -80,6 +154,5 @@ return graph;
 
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  
+  console.log(`Server is running on http://localhost:${port}`); 
 });
